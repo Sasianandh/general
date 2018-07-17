4286f7a0f5fe3dc32e0b00793869312d874e1177
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-07-17 17:00:53 +0530

    Create hack1.c

---
+++ hack1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    long a[10000],b[10000],i,j,k=0,n,sum=0;
+    scanf("%ld",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&a[i]);
+    }
+     for(i=0;i<n;i++)
+    {
+  for(j=0;j<n;j++)
+    {
+      if(i!=j)
+      {
+          sum=sum+a[j];
+         }
+  }
+       b[k]=sum;
+          k++;
+         sum=0;
+      
+  }
+          for(i=0;i<k;i++)
+          {
+             for(j=0;j<k;j++)
+          {  
+                 if(a[i]<)
+          }
+    {
+}
