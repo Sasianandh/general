e4f2edb2f28864929d784c67cdbb28b0431b5fed
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-07-20 11:31:45 +0530

    Update hack1.c

--- hack1.c
+++ hack1.c
@@ -1,38 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    long a[10000],b[10000],i,j,k=0,n,sum=0;
-    scanf("%ld",&n);
+    long n,a[100000],i,j,k,l,s=0;
     for(i=0;i<n;i++)
     {
         scanf("%ld",&a[i]);
     }
-     for(i=0;i<n;i++)
-    {
-  for(j=0;j<n;j++)
-    {
-      if(i!=j)
-      {
-          sum=sum+a[j];
-         }
-  }
-       b[k]=sum;
-          k++;
-         sum=0;
-      
-  }
-          for(i=0;i<k;i++)
-          {
-             for(j=0;j<k;j++)
-          {  
-                 if(a[i]<a[j])
-                 {
-                     t=a[i];
-                     a[i]=a[j];
-                     a[j]=t;
-          }
-             }
-          }
-    printf("%d %d",a[0],a[n-1]);
-    return 0;
+    
 }
