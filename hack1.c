f365e157438c76ebcc4603126853b807f78bf83e
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-07-20 16:56:24 +0530

    Update hack1.c

--- hack1.c
+++ hack1.c
@@ -1,10 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    long n,a[100000],i,j,k,l,s=0;
+    long a[100],n,i,j,k=0,l,s=0;
     for(i=0;i<n;i++)
     {
         scanf("%ld",&a[i]);
     }
-    
+    for(i=0;i<n;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(a[i]==j)
+                k++;
+        }
+        if(k>s)
+        {
+        s=k;
+        }
+        }
+    printf("%ld",s);
+    return 0;
 }
